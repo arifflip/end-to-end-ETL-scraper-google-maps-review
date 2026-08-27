af6337fc65780901af12b0ec6bd00dc9502921f6
arifflip <newarh14@gmail.com>
2026-08-27 16:39:57 +0700

    add - DAG with transformation task for silver and gold layer

---
+++ dags/DAG_transformation.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from datetime import datetime
+from airflow.decorators import dag, task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.hooks.base import BaseHook
+from sqlalchemy import create_engine
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+
+default_args = {
+    "owner": "airflow",
+    "retries": 0,
+}
+
+@dag(
+    dag_id="transformation_silver_and_gold",
+    default_args=default_args,
+    start_date=datetime(2026, 1, 1),
+    schedule_interval=None, # Dijalankan manual lewat UI Airflow
+    catchup=False,
+    tags=["Ingestion", "Postgres", "Scraper"],
+)
+
+def silver_gold_transformation() :
+
+    task_silver_and_gold_transformation = SQLExecuteQueryOperator(
+        task_id='task_silver_and_gold_transformation',
+        conn_id='dwh_postgres',
+        sql='function_scraper_and_integration/transformation_silver_and_gold.sql',)
+
+    task_silver_and_gold_transformation
+
+silver_gold_transformation()
